17fb695761053f4083fb555238fa8456622f7828
Istvan Kovacs <istvan.kovacs@migeran.com>
2016-09-09 13:15:33 +0200

    Fixed logger configration, added some logs

--- moe/tools/moe.plugin.idea/src/main/java/org/moe/idea/utils/logger/LoggerFactory.java
+++ moe/tools/moe.plugin.idea/src/main/java/org/moe/idea/utils/logger/LoggerFactory.java
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
 package org.moe.idea.utils.logger;
 
 import com.intellij.openapi.diagnostic.Logger;
@@ -24,34 +23,8 @@ import java.util.Properties;
 
 public class LoggerFactory {
 
-    /**
-     * Configured flag.
-     */
-    private static boolean isConfigured;
-
-    /**
-     * Log4j filename.
-     */
-    private static final String LOG4J_FILENAME = "log4j.properties";
-
     public static Logger getLogger(Class classname) {
-        configure();
         return Logger.getInstance(classname);
     }
 
-    private synchronized static void configure() {
-        if (isConfigured) {
-            return;
-        }
-
-        Properties p = new Properties();
-        p.setProperty("log4j.rootLogger", "ERROR, stdout");
-        p.setProperty("log4j.appender.stdout", "org.apache.log4j.ConsoleAppender");
-        p.setProperty("log4j.appender.stdout.layout", "org.apache.log4j.PatternLayout");
-        p.setProperty("log4j.logger.#org.moe.mdt", "DEBUG");
-        p.setProperty("log4j.logger.org.moe.mdt", "DEBUG");
-        PropertyConfigurator.configure(p);
-
-        isConfigured = true;
-    }
 }
