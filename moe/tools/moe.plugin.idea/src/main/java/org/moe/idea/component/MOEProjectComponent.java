944db4ef6b86265878aa65e7f7f34e12e05d8645
Istvan Kovacs <istvan.kovacs@migeran.com>
2016-09-14 16:39:39 +0200

    Automatically created run configurations for the all MOE modules in the opened project

--- moe/tools/moe.plugin.idea/src/main/java/org/moe/idea/component/MOEProjectComponent.java
+++ moe/tools/moe.plugin.idea/src/main/java/org/moe/idea/component/MOEProjectComponent.java
@@ -16,9 +16,14 @@ limitations under the License.
 
 package org.moe.idea.component;
 
+import com.intellij.execution.RunManager;
+import com.intellij.execution.RunnerAndConfigurationSettings;
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.compiler.CompileTask;
 import com.intellij.openapi.compiler.CompilerManager;
 import com.intellij.openapi.components.AbstractProjectComponent;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
@@ -27,11 +32,17 @@ import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.util.messages.MessageBusConnection;
 import org.moe.idea.MOESdkPlugin;
 import org.moe.idea.compiler.MOECompileTask;
+import org.moe.idea.runconfig.configuration.MOERunConfiguration;
+import org.moe.idea.runconfig.configuration.MOERunConfigurationType;
+import org.moe.idea.ui.MOEToolWindow;
 import org.moe.idea.utils.FileEditorListener;
-import org.moe.idea.utils.ModuleUtils;
 import org.moe.idea.utils.XCodeProjectSyncListener;
+import org.moe.idea.utils.logger.LoggerFactory;
 
 public class MOEProjectComponent extends AbstractProjectComponent {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MOEProjectComponent.class);
+
     private MessageBusConnection messageBusConnection;
 
     protected MOEProjectComponent(Project project) {
@@ -72,11 +83,29 @@ public class MOEProjectComponent extends AbstractProjectComponent {
     @Override
     public void projectOpened() {
         installCompileTask();
-
+        // Find MOE modules
         for (Module module : ModuleManager.getInstance(myProject).getModules()) {
             if (MOESdkPlugin.isValidMoeModule(module)) {
                 if (module.getModuleFile() == null){
                     module.getProject().save();
+                }
+                // Find run config for the module
+                RunManager runManager = RunManager.getInstance(myProject);
+                for (RunConfiguration runConfig : runManager.getConfigurationsList(MOERunConfigurationType.getInstance())) {
+                    MOERunConfiguration moeRunConfig = (MOERunConfiguration) runConfig;
+                    moeRunConfig.moduleName(moeRunConfig.moduleName());// Set module in the config
+                    if (module.getName().equals(moeRunConfig.module().getName())) {
+                        return;
+                    };
+                }
+                // Create MOE run config
+                RunnerAndConfigurationSettings settings = null;
+                try {
+                    settings = MOERunConfiguration.createRunConfiguration(myProject, module);
+                    runManager.addConfiguration(settings, false);
+                    runManager.setSelectedConfiguration(settings);
+                } catch (Exception e) {
+                    LOG.error("Unable to create run configuration", e);
                 }
             }
         }
