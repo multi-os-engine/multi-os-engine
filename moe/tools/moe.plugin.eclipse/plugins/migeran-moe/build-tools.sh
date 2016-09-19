4ea05be0d1ec4d74baca7a8a071f9576636468b4
Roland Vigh <roland.vigh@migeran.com>
2016-09-19 11:20:01 +0200

    Added missed dependencies

--- moe/tools/moe.plugin.eclipse/plugins/migeran-moe/build-tools.sh
+++ moe/tools/moe.plugin.eclipse/plugins/migeran-moe/build-tools.sh
@@ -3,5 +3,7 @@ cd ../../../
 rm -r ./moe.tools.natjgen/build
 rm -r ./moe.tools.common/build
 rm -r ./moe.generator.project/build
+rm -r ./moe.document.xib/build
+rm -r ./moe.document.pbxproj/build
 cd master
 ./gradlew :moe.plugin.idea:build
