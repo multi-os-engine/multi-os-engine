fff84756a6fbd41630a9280ab41320166ff1d6b0
Kristof Liliom <kristof.liliom@migeran.com>
2016-12-14 18:24:27 +0100

    Added new keys to ProjectUtil, removed key specific helper methods

--- moe/tools/moe.tools.common/src/main/java/org/moe/common/utils/ProjectUtil.java
+++ moe/tools/moe.tools.common/src/main/java/org/moe/common/utils/ProjectUtil.java
@@ -24,48 +24,20 @@ import java.util.Properties;
 
 public class ProjectUtil {
 
-    public static final String XCODE_PROJECT_PATH_KEY = "moe.xcode.xcodeProjectPath";
-    public static final String XCODE_PROJECT_PATH_TASK = "moeXcodeProperties";
+    public static final String XCODE_PROPERTIES_TASK = "moeXcodeProperties";
+    public static final String SDK_PROPERTIES_TASK = "moeSDKProperties";
+
+    public static final String XCODE_PROJECT_KEY = "moe.xcode.project";
+    public static final String XCODE_WORKSPACE_KEY = "moe.xcode.workspace";
     public static final String XCODE_MAIN_TARGET_KEY = "moe.xcode.mainTarget";
     public static final String XCODE_TEST_TARGET_KEY = "moe.xcode.testTarget";
-    public static final String SDK_PROPERTIES_TASK = "moeSDKProperties";
+    public static final String XCODE_MAIN_SCHEME_KEY = "moe.xcode.mainScheme";
+    public static final String XCODE_TEST_SCHEME_KEY = "moe.xcode.testScheme";
     public static final String SDK_PATH_KEY = "moe.sdk.home";
-
-    public static String retrieveXcodeProjectPathFromGradle(File projectFile) {
-        return retrievePropertyFromGradle(projectFile, XCODE_PROJECT_PATH_TASK, XCODE_PROJECT_PATH_KEY);
-    }
-
-    public static String retrieveSDKPathFromGradle(File projectFile) {
-        return retrievePropertyFromGradle(projectFile, SDK_PROPERTIES_TASK, SDK_PATH_KEY);
-    }
-
-    private static String retrievePropertyFromGradle(File projectFile, String taskName, String modulePropertyKey) {
-        String property;
-
-        GradleExec exec = new GradleExec(projectFile, null, projectFile);
-        exec.getArguments().add(taskName);
-        exec.getArguments().add("-Dorg.gradle.daemon=true");
-        exec.getArguments().add("-Dorg.gradle.configureondemand=true");
-
-        String keyWord = modulePropertyKey + '=';
-
-        try {
-            property = ExecOutputCollector.collect(exec);
-            property = property.substring(property.lastIndexOf(keyWord) + keyWord.length()).replace("\t", "");
-            property = property.substring(0, property.indexOf('\n'));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            property = null;
-        }
-
-        return property;
-    }
 
     public static Properties retrievePropertiesFromGradle(File projectFile, String taskName) {
         GradleExec exec = new GradleExec(projectFile, null, projectFile);
         exec.getArguments().add(taskName);
-        exec.getArguments().add("-Dorg.gradle.daemon=true");
-        exec.getArguments().add("-Dorg.gradle.configureondemand=true");
 
         final Properties properties = new Properties();
         try {
