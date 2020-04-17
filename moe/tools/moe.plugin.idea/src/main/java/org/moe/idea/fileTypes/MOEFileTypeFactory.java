9cfa6fe867149cad9f1ee388cb124135170e0003
Noisyfox <timemanager.rick@gmail.com>
2020-04-17 12:24:13 +1000

    Add NBC file type

--- moe/tools/moe.plugin.idea/src/main/java/org/moe/idea/fileTypes/MOEFileTypeFactory.java
+++ moe/tools/moe.plugin.idea/src/main/java/org/moe/idea/fileTypes/MOEFileTypeFactory.java
@@ -1,5 +1,6 @@
 package org.moe.idea.fileTypes;
 
+import com.intellij.json.JsonFileType;
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
 import com.intellij.openapi.fileTypes.UserFileType;
@@ -8,10 +9,10 @@ import org.jetbrains.annotations.NotNull;
 
 public class MOEFileTypeFactory extends FileTypeFactory {
 
-    private static final UserFileType xcodeEditorFileType;
+    private static final XcodeEditorFileType xcodeEditorFileType = new XcodeEditorFileType();
+    private static final BindingEditorFileType bindingEditorFileType = new BindingEditorFileType();
 
     static {
-        xcodeEditorFileType = new XcodeEditorFileType();
         xcodeEditorFileType.setName("Xcode project");
         xcodeEditorFileType.setDescription("Xcode project file");
     }
@@ -19,6 +20,7 @@ public class MOEFileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
         fileTypeConsumer.consume(xcodeEditorFileType, "pbxproj");
+        fileTypeConsumer.consume(bindingEditorFileType, "nbc");
     }
 
     public static final class XcodeEditorFileType extends UserFileType<XcodeEditorFileType> {
@@ -31,6 +33,27 @@ public class MOEFileTypeFactory extends FileTypeFactory {
         @Override
         public boolean isBinary() {
             return false;
+        }
+    }
+
+    public static final class BindingEditorFileType extends JsonFileType {
+
+        @NotNull
+        @Override
+        public String getName() {
+            return "NatJ Binding Config";
+        }
+
+        @NotNull
+        @Override
+        public String getDescription() {
+            return "NatJ Binding Config File";
+        }
+
+        @NotNull
+        @Override
+        public String getDefaultExtension() {
+            return "nbc";
         }
     }
 }
