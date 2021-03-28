893b4aa60bf967665542c2fb6bc139712cb9d863
Noisyfox <timemanager.rick@gmail.com>
2021-03-28 12:30:54 +1100

    Only pack application resources

--- moe/tools/moe.plugin.gradle/src/main/java/org/moe/gradle/tasks/ResourcePackager.java
+++ moe/tools/moe.plugin.gradle/src/main/java/org/moe/gradle/tasks/ResourcePackager.java
@@ -23,7 +23,6 @@ import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.bundling.Jar;
 import org.moe.gradle.MoeExtension;
 import org.moe.gradle.MoePlugin;
-import org.moe.gradle.MoeSDK;
 import org.moe.gradle.anns.NotNull;
 import org.moe.gradle.groovy.closures.ConfigurationClosure;
 import org.moe.gradle.utils.Require;
@@ -78,7 +77,6 @@ public class ResourcePackager {
 
         final Project project = plugin.getProject();
         final MoeExtension ext = plugin.getExtension();
-        final MoeSDK sdk = plugin.getSDK();
 
         // Construct default output path
         final Path out = Paths.get(MoePlugin.MOE, sourceSet.getName());
@@ -95,26 +93,14 @@ public class ResourcePackager {
 
         // Update settings
         resourcePackagerTask.setDestinationDir(project.file(project.getBuildDir().toPath().resolve(out).toFile()));
-        resourcePackagerTask.setArchiveName("application.jar");
+        resourcePackagerTask.setArchiveName("resource.jar");
+        // For SubstrateVM, the resources are directly picked up from the classpath,
+        // so there is no need to gather them into a single jar. We only need to gather those
+        // picked up by proguard.
         resourcePackagerTask.from(project.zipTree(proguardTask.getOutJar()));
         resourcePackagerTask.exclude("**/*.class");
 
         project.afterEvaluate(_project -> {
-            // When using full trim, ProGuard will copy the the resources from the common jar
-            switch (ext.getProguardLevelRaw()) {
-                case MoeExtension.PROGUARD_LEVEL_APP:
-//                    resourcePackagerTask.from(_project.zipTree(sdk.getCoreJar()));
-                    resourcePackagerTask.from(_project.zipTree(ext.getPlatformJar()));
-                    break;
-                case MoeExtension.PROGUARD_LEVEL_PLATFORM:
-//                    resourcePackagerTask.from(_project.zipTree(sdk.getCoreJar()));
-                    break;
-                case MoeExtension.PROGUARD_LEVEL_ALL:
-                    break;
-                default:
-                    throw new IllegalStateException();
-            }
-
             ext.packaging.getExcludes().forEach(resourcePackagerTask::exclude);
 
             // Add support for copying resources from the source directory
