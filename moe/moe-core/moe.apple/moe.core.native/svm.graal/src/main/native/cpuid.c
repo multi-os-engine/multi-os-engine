13bb97e65234e647b20b063654622c1085291d20
Noisyfox <timemanager.rick@gmail.com>
2021-04-11 17:29:58 +1000

    Add dummy cpuid.c

---
+++ moe/moe-core/moe.apple/moe.core.native/svm.graal/src/main/native/cpuid.c
@@ -0,0 +1,15 @@
+//
+//  cpuid.c
+//  libjvm
+//
+//  Created by Noisyfox on 11/4/21.
+//
+
+#include <stdio.h>
+
+/*
+ * Dummy for iOS
+ */
+void determineCPUFeatures(void* features) {
+    fprintf(stderr, "\n\n\ndetermineCpuFeaures\n");
+}
