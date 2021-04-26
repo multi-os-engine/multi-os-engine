3705733582e31e1a3bab781f569917da4a1ebe5a
Noisyfox <timemanager.rick@gmail.com>
2021-04-27 02:09:19 +1000

    A util class for gather all used types from a given classpath

---
+++ moe/tools/moe.tools.classvalidator/src/main/kotlin/org/moe/tools/classvalidator/substrate/TypeCollector.kt
@@ -0,0 +1,35 @@
+package org.moe.tools.classvalidator.substrate
+
+import org.moe.common.utils.classAndJarInputIterator
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.commons.ClassRemapper
+import org.objectweb.asm.commons.Remapper
+import java.io.File
+
+object TypeCollector {
+    fun process(
+        classpath: Set<File>,
+        visitor: (type: String, external: Boolean) -> Unit,
+    ) {
+        val internalClasses: MutableSet<String> = mutableSetOf()
+        val allClasses: MutableSet<String> = sortedSetOf()
+
+        val remapper = object : Remapper() {
+            override fun map(internalName: String): String {
+                allClasses.add(internalName)
+                return super.map(internalName)
+            }
+        }
+
+        classpath.classAndJarInputIterator {
+            val cr = ClassReader(it)
+            internalClasses.add(cr.className)
+            cr.accept(ClassRemapper(ClassWriter(0), remapper), 0)
+        }
+
+        allClasses.forEach {
+            visitor(it, it !in internalClasses)
+        }
+    }
+}
