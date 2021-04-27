823f213bf2690d24ab3ff95a84a06d2801eb0d3a
Noisyfox <timemanager.rick@gmail.com>
2021-04-27 12:31:41 +1000

    Also analysis the usage of each type

--- moe/tools/moe.tools.classvalidator/src/main/kotlin/org/moe/tools/classvalidator/utils/TypeUsageCollector.kt
+++ moe/tools/moe.tools.classvalidator/src/main/kotlin/org/moe/tools/classvalidator/utils/TypeUsageCollector.kt
@@ -1,4 +1,4 @@
-package org.moe.tools.classvalidator.substrate
+package org.moe.tools.classvalidator.utils
 
 import org.moe.common.utils.classAndJarInputIterator
 import org.objectweb.asm.ClassReader
@@ -11,17 +11,17 @@ import java.io.File
  * A simple util that collect all classes that has been used by the classes from given classpath.
  * The used types include external classes as well as the classes from the classpath.
  */
-object TypeCollector {
+object TypeUsageCollector {
     fun process(
         classpath: Set<File>,
-        visitor: (type: String, external: Boolean) -> Unit,
+        visitor: (type: String, usages: Int, external: Boolean) -> Unit,
     ) {
         val internalClasses: MutableSet<String> = mutableSetOf()
-        val allClasses: MutableSet<String> = sortedSetOf()
+        val allClasses: MutableMap<String, Int> = sortedMapOf()
 
         val remapper = object : Remapper() {
             override fun map(internalName: String): String {
-                allClasses.add(internalName)
+                allClasses[internalName] = 0 // TODO
                 return super.map(internalName)
             }
         }
@@ -32,8 +32,20 @@ object TypeCollector {
             cr.accept(ClassRemapper(ClassWriter(0), remapper), 0)
         }
 
-        allClasses.forEach {
-            visitor(it, it !in internalClasses)
+        allClasses.forEach { (name, usage) ->
+            visitor(name, usage, name !in internalClasses)
         }
     }
+
+    /** This type is used as a super type */
+    const val USAGE_SUPER = 1 shl 0
+
+    /** This type is used as a method parameter type */
+    const val USAGE_PARAM = 1 shl 1
+
+    /** This type is used as a method return type */
+    const val USAGE_RETURN = 1 shl 2
+
+    /** This type is used as a variable type */
+    const val USAGE_VARIABLE = 1 shl 3
 }
