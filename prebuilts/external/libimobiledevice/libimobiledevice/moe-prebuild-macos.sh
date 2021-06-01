c24b2f54451e9c823d2b7c0ccab19403600127b7
Noisyfox <timemanager.rick@gmail.com>
2021-06-02 02:12:35 +1000

    Add support for apple silicon

--- prebuilts/external/libimobiledevice/libimobiledevice/moe-prebuild-macos.sh
+++ prebuilts/external/libimobiledevice/libimobiledevice/moe-prebuild-macos.sh
@@ -22,15 +22,27 @@ git apply libimobiledevice-fix-ssl_read_with_timeout.patch
 __MOE_TARGET="$MOE_PREBUILTS_DIR/$MOE_PREBUILTS_TARGET_DIR"
 __MOE_PREBUILTS_DIR="$MOE_PREBUILTS_DIR/external/libimobiledevice/libplist/build/macos/lib/pkgconfig"
 __MOE_PREBUILTS_DIR="$__MOE_PREBUILTS_DIR:$MOE_PREBUILTS_DIR/external/libimobiledevice/libusbmuxd/build/macos/lib/pkgconfig"
-__MOE_PREBUILTS_DIR="$__MOE_PREBUILTS_DIR:$MOE_PREBUILTS_DIR/external/libimobiledevice/openssl/build/macos/lib/pkgconfig"
 
 ./autogen.sh \
---prefix="$__MOE_TARGET" \
+--prefix="$__MOE_TARGET/x86_64" \
 --disable-shared \
-CFLAGS="-DMOE"\
-" -I$MOE_PREBUILTS_DIR/external/libimobiledevice/openssl/build/macos/include"\
+CFLAGS="-DMOE -arch x86_64"\
+" -I$MOE_PREBUILTS_DIR/external/libimobiledevice/openssl/build/macos/x86_64/include"\
 " -I$MOE_PREBUILTS_DIR/external/libimobiledevice/libusbmuxd/build/macos/include" \
 --without-cython \
-PKG_CONFIG_LIBDIR="$__MOE_PREBUILTS_DIR"
+PKG_CONFIG_LIBDIR="$__MOE_PREBUILTS_DIR:$MOE_PREBUILTS_DIR/external/libimobiledevice/openssl/build/macos/x86_64/lib/pkgconfig"
+make
+make install
+
+make clean
+
+./autogen.sh \
+--prefix="$__MOE_TARGET/arm64" \
+--disable-shared \
+CFLAGS="-DMOE -arch arm64"\
+" -I$MOE_PREBUILTS_DIR/external/libimobiledevice/openssl/build/macos/arm64/include"\
+" -I$MOE_PREBUILTS_DIR/external/libimobiledevice/libusbmuxd/build/macos/include" \
+--without-cython \
+PKG_CONFIG_LIBDIR="$__MOE_PREBUILTS_DIR:$MOE_PREBUILTS_DIR/external/libimobiledevice/openssl/build/macos/arm64/lib/pkgconfig"
 make
 make install
