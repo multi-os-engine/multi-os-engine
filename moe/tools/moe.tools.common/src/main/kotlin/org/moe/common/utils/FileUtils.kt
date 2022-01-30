f67f70abe58fb3e3c4b8c7f37b35893096a7d76c
Noisyfox <timemanager.rick@gmail.com>
2022-01-30 16:26:46 +0800

    Allow iterating classpath without opening input stream

--- moe/tools/moe.tools.common/src/main/kotlin/org/moe/common/utils/FileUtils.kt
+++ moe/tools/moe.tools.common/src/main/kotlin/org/moe/common/utils/FileUtils.kt
@@ -6,38 +6,77 @@ import java.io.InputStream
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
+import java.util.jar.JarEntry
 import java.util.jar.JarFile
 
-fun File.classpathIterator(consumer: (path: String, inputStream: InputStream) -> Unit, filter: (path: String) -> Boolean) {
+sealed class ClasspathEntry {
+    abstract val path: String
+    abstract val isDirectory: Boolean
+
+    abstract fun inputStream(): InputStream
+
+    internal class Jar(
+        private val jarFile: JarFile,
+        private val jarEntry: JarEntry,
+    ) : ClasspathEntry() {
+        override val path: String
+            get() = jarEntry.name
+
+        override val isDirectory: Boolean
+            get() = jarEntry.isDirectory
+
+        override fun inputStream(): InputStream = jarFile.getInputStream(jarEntry)
+    }
+
+    internal class NormalFile(
+        private val file: File,
+        override val path: String
+    ) : ClasspathEntry() {
+        override val isDirectory: Boolean
+            get() = file.isDirectory
+
+        override fun inputStream(): InputStream = file.inputStream()
+    }
+}
+
+fun File.classpathIterator(consumer: (entry: ClasspathEntry) -> Unit) {
     if (!this.exists()) {
         return
     }
 
     if (this.isDirectory) {
         this.walk().forEach walk@{ f ->
-            if (f.isDirectory) {
-                return@walk
-            }
-
-            val relPath = f.relativeToOrSelf(this)
-
-            if (!filter(relPath.path)) {
-                return@walk
-            }
-
-            f.inputStream().use {
-                it.reader()
-                consumer(relPath.path, it) }
+            consumer(
+                ClasspathEntry.NormalFile(
+                    file = f,
+                    path = f.relativeToOrSelf(this).path,
+                )
+            )
         }
     } else if (this.name.endsWith(".jar")) {
-        val file = JarFile(this)
-
-        file.stream().forEach jar@{ entry ->
-            if (entry.isDirectory || !filter(entry.name)) {
-                return@jar
+        JarFile(this).use { file ->
+            file.stream().forEach jar@{ entry ->
+                consumer(
+                    ClasspathEntry.Jar(
+                        jarFile = file,
+                        jarEntry = entry,
+                    )
+                )
             }
+        }
+    }
+}
 
-            file.getInputStream(entry).use { consumer(entry.name, it) }
+fun Iterable<File>.classpathIterator(consumer: (entry: ClasspathEntry) -> Unit) = forEach {
+    it.classpathIterator(consumer)
+}
+
+fun File.classpathIterator(consumer: (path: String, inputStream: InputStream) -> Unit, filter: (path: String) -> Boolean) {
+    this.classpathIterator { entry->
+        if(!entry.isDirectory && filter(entry.path)) {
+            entry.inputStream().use {
+                consumer(entry.path, it)
+            }
         }
     }
 }
