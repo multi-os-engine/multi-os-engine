6eb7e3eced5e64897e0e1da1c177d70c75e6bb45
Noisyfox <timemanager.rick@gmail.com>
2022-07-19 13:38:17 +1000

    Pass MOE plugin and SDK versions via gradle tooling model

--- moe/tools/moe.plugin.idea/src/main/kotlin/org/moe/idea/model/impl/MOESdkPropertiesImpl.kt
+++ moe/tools/moe.plugin.idea/src/main/kotlin/org/moe/idea/model/impl/MOESdkPropertiesImpl.kt
@@ -1,16 +1,19 @@
 package org.moe.idea.model.impl
 
 import org.moe.gradle.model.MOESdkProperties
+import org.moe.idea.extension.MOEGradleProjectResolver.Companion.readGradleModelOptionalProperty
 import java.io.Serializable
 
 data class MOESdkPropertiesImpl(
     override val home: String,
+    override val version: String?,
     override val coreJar: String,
     override val platformJar: String?,
-    override val junitJar: String
+    override val junitJar: String,
 ) : MOESdkProperties, Serializable {
     constructor(input: MOESdkProperties) : this(
         home = input.home,
+        version = input.readGradleModelOptionalProperty { version },
         coreJar = input.coreJar,
         platformJar = input.platformJar,
         junitJar = input.junitJar
