a35dece524aab1be0ab5a2ad834d72e4999fcd27
Berstanio <berstaniodev@gmail.com>
2023-06-27 22:55:52 +0200

    fix: Adjust for new graalvm versioning since 23.0

--- moe/tools/moe.plugin.idea/src/main/kotlin/org/moe/idea/model/impl/GraalVMPropertiesImpl.kt
+++ moe/tools/moe.plugin.idea/src/main/kotlin/org/moe/idea/model/impl/GraalVMPropertiesImpl.kt
@@ -1,43 +1,17 @@
 package org.moe.idea.model.impl
 
 import org.moe.gradle.model.GraalVMProperties
-import org.moe.gradle.model.GraalVMVersion
 import org.moe.gradle.model.JDKVersion
 import java.io.Serializable
 
 data class GraalVMPropertiesImpl(
     override val home: String,
-    override val version: GraalVMVersion,
     override val jdkVersion: JDKVersion
 ) : GraalVMProperties, Serializable {
     constructor(input: GraalVMProperties) : this(
         home = input.home,
-        version = GraalVMVersionImpl(input.version),
         jdkVersion = JDKVersionImpl(input.jdkVersion),
     )
-}
-
-data class GraalVMVersionImpl(
-    override val year: Int,
-    override val feature: Int,
-    override val patch: Int,
-    override val bugfix: Int
-) : GraalVMVersion, Serializable {
-    constructor(input: GraalVMVersion) : this(
-        year = input.year,
-        feature = input.feature,
-        patch = input.patch,
-        bugfix = input.bugfix,
-    )
-
-    override fun toString(): String {
-        val base = "$year.$feature.$patch"
-        return if (bugfix > 0) {
-            return "$base.$bugfix"
-        } else {
-            base
-        }
-    }
 }
 
 data class JDKVersionImpl(
