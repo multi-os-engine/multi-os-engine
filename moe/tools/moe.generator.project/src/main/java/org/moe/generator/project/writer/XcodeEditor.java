eace39caf819e52af9d1e96a6828f876479aabf7
Berstanio <berstaniodev@gmail.com>
2023-10-20 12:31:39 +0200

    fix: Don't link against UniformType framework, since it is iOS 14+

--- moe/tools/moe.generator.project/src/main/java/org/moe/generator/project/writer/XcodeEditor.java
+++ moe/tools/moe.generator.project/src/main/java/org/moe/generator/project/writer/XcodeEditor.java
@@ -310,7 +310,7 @@ public class XcodeEditor extends AbstractXcodeEditor {
             "${MOE_CUSTOM_OTHER_LDFLAGS} "
                 + "-Wl,-force_load,${MOE_LIB_PATH}/libmoe.a "
                 + "-lc++ -lpthread -lsqlite3 "
-                + "-Wl,-framework,Foundation -Wl,-framework,UniformTypeIdentifiers -Wl,-framework,CoreServices"
+                + "-Wl,-framework,Foundation -Wl,-framework,CoreServices"
         );
         setBuildSetting(target, "MOE_OTHER_LDFLAGS", sb.toString());
 
