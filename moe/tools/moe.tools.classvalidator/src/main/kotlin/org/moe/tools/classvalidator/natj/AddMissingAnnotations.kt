multi-os-engine/moe-tools-classvalidator

fix: Use ASM9, it is supported by AGP since version 7

src/main/kotlin/org/moe/tools/classvalidator/natj/AddMissingAnnotations.kt
@@ -12,7 +12,7 @@ import java.lang.reflect.AnnotatedElement
 
 class AddMissingAnnotations(
     next: ClassVisitor?
-) : ClassModifier(Opcodes.ASM5, next) {
+) : ClassModifier(Opcodes.ASM9, next) {
     private var skip: Boolean = false
     private var superName: String? = null
     private var interfaces: Array<out String>? = null
@@ -50,7 +50,7 @@ class AddMissingAnnotations(
             AnnotationHelper(it, true)
         }
 
-        return object : MethodVisitor(Opcodes.ASM5, visitor) {
+        return object : MethodVisitor(Opcodes.ASM9, visitor) {
             var isVisiting = true
 
             override fun visitAnnotation(descriptor: String, visible: Boolean): AnnotationVisitor? {
@@ -150,15 +150,5 @@ class AddMissingAnnotations(
                 }
             }
         }
-    }
-
-    override fun visitNestHost(nestHost: String?) {
-        // Do nothing
-        // Fix error "NestHost requires ASM7" without the need of updating to ASM7
-    }
-
-    override fun visitNestMember(nestMember: String?) {
-        // Do nothing
-        // Fix error "NestMember requires ASM7" without the need of updating to ASM7
     }
 }

src/main/kotlin/org/moe/tools/classvalidator/natj/AddMissingNatJRegister.kt
@@ -15,7 +15,7 @@ import java.lang.reflect.Modifier
  */
 class AddMissingNatJRegister(
     next: ClassVisitor?
-) : ClassModifier(Opcodes.ASM5, next) {
+) : ClassModifier(Opcodes.ASM9, next) {
 
     private var skip: Boolean = false
     private var visit: Boolean = false
@@ -115,16 +115,6 @@ class AddMissingNatJRegister(
         }
 
         super.visitEnd()
-    }
-
-    override fun visitNestHost(nestHost: String?) {
-        // Do nothing
-        // Fix error "NestHost requires ASM7" without the need of updating to ASM7
-    }
-
-    override fun visitNestMember(nestMember: String?) {
-        // Do nothing
-        // Fix error "NestMember requires ASM7" without the need of updating to ASM7
     }
 
     companion object {

multi-os-engine/multi-os-engine

moe/tools/moe.tools.classvalidator/src/main/kotlin/org/moe/tools/classvalidator/natj/AddMissingAnnotations.kt
@@ -11,7 +11,7 @@ import java.lang.reflect.AnnotatedElement
 
 class AddMissingAnnotations(
     next: ClassVisitor?
-) : ClassVisitor(Opcodes.ASM5, next) {
+) : ClassVisitor(Opcodes.ASM9, next) {
     private var skip: Boolean = false
     private var superName: String? = null
     private var interfaces: Array<out String>? = null
@@ -49,7 +49,7 @@ class AddMissingAnnotations(
             AnnotationHelper(it, true)
         }
 
-        return object : MethodVisitor(Opcodes.ASM5, visitor) {
+        return object : MethodVisitor(Opcodes.ASM9, visitor) {
             var isVisiting = true
 
             override fun visitAnnotation(descriptor: String, visible: Boolean): AnnotationVisitor? {

moe/tools/moe.tools.classvalidator/src/main/kotlin/org/moe/tools/classvalidator/natj/AddMissingNatJRegister.kt
@@ -13,7 +13,7 @@ import org.objectweb.asm.tree.MethodNode
  */
 class AddMissingNatJRegister(
     next: ClassVisitor?
-) : ClassVisitor(Opcodes.ASM5, next) {
+) : ClassVisitor(Opcodes.ASM9, next) {
 
     private var skip: Boolean = false
     private var visit: Boolean = false