a2f00584157aaefe48f58102d404926e5c46f8ec
Berstanio <berstaniodev@gmail.com>
2024-08-27 21:38:45 +0200

    chore: Try adjust all prebuilts references

--- moe/natj/natj/natj-ndk/jni/Android.mk
+++ moe/natj/natj/natj-ndk/jni/Android.mk
@@ -14,45 +14,45 @@ LOCAL_CPP_FEATURES := \
   exceptions
 
 ifeq ($(TARGET_ARCH), arm)
-  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/arm/lib/libffi.a -Wl,--no-whole-archive
+  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/arm/lib/libffi.a -Wl,--no-whole-archive
   LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../../src/native/include \
-    $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/arm/lib/libffi-3.99999/include
+    $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/arm/lib/libffi-3.99999/include
 endif
 
 ifeq ($(TARGET_ARCH), mips)
-  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/mipsel/lib/libffi.a -Wl,--no-whole-archive
+  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/mipsel/lib/libffi.a -Wl,--no-whole-archive
   LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../../src/native/include \
-    $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/mipsel/lib/libffi-3.99999/include
+    $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/mipsel/lib/libffi-3.99999/include
 endif
 
 ifeq ($(TARGET_ARCH), x86)
-  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/x86/lib/libffi.a -Wl,--no-whole-archive
+  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/x86/lib/libffi.a -Wl,--no-whole-archive
   LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../../src/native/include \
-    $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/x86/lib/libffi-3.99999/include
+    $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/x86/lib/libffi-3.99999/include
 endif
 
 ifeq ($(TARGET_ARCH), arm64)
-  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/aarch64/lib64/libffi.a -Wl,--no-whole-archive
+  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/aarch64/lib64/libffi.a -Wl,--no-whole-archive
   LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../../src/native/include \
-    $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/aarch64/lib/libffi-3.99999/include
+    $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/aarch64/lib/libffi-3.99999/include
 endif
 
 ifeq ($(TARGET_ARCH), mips64)
-  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/mips64el/lib64/libffi.a -Wl,--no-whole-archive
+  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/mips64el/lib64/libffi.a -Wl,--no-whole-archive
   LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../../src/native/include \
-    $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/mips64el/lib/libffi-3.99999/include
+    $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/mips64el/lib/libffi-3.99999/include
 endif
 
 ifeq ($(TARGET_ARCH), x86_64)
-  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/x86_64/lib64/libffi.a -Wl,--no-whole-archive
+  LOCAL_LDLIBS := -Wl,--whole-archive $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/x86_64/lib64/libffi.a -Wl,--no-whole-archive
   LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../../src/native/include \
-    $(LOCAL_PATH)/../../../../../prebuilts/external/libffi/build/ndk/x86_64/lib/libffi-3.99999/include
+    $(LOCAL_PATH)/../../../../prebuilts/external/libffi/build/ndk/x86_64/lib/libffi-3.99999/include
 endif
 
 ifeq ($(LOCAL_LDLIBS),)
