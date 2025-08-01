34e6b10d2104932de6d220e81b1fb893f1ac6a20
Berstanio <berstaniodev@gmail.com>
2025-08-01 16:59:51 +0200

    fix: Fix incorrect extension function resolve

--- tools/moe.tools.substrate/src/main/kotlin/org/moe/tools/substrate/utils/File.kt
+++ tools/moe.tools.substrate/src/main/kotlin/org/moe/tools/substrate/utils/File.kt
@@ -3,20 +3,19 @@ package org.moe.tools.substrate.utils
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
-import kotlin.io.path.extension
-import kotlin.streams.toList
+import java.util.stream.Collectors
 
 @Throws(IOException::class)
 fun Path.find(fileName: String, isDirectory: Boolean = false, maxDepth: Int = Int.MAX_VALUE): List<Path> {
     return Files.find(this, maxDepth, { p, _ ->
         p.fileName.toString() == fileName && (isDirectory == Files.isDirectory(p))
-    }).toList()
+    }).collect(Collectors.toList())
 }
 
 fun Path.findByExt(extension: String, maxDepth: Int = Int.MAX_VALUE): List<Path> {
     return Files.find(this, maxDepth, { p, _ ->
         p.toFile().extension == extension && !Files.isDirectory(p)
-    }).toList()
+    }).collect(Collectors.toList())
 }
 
 @Throws(IOException::class)
