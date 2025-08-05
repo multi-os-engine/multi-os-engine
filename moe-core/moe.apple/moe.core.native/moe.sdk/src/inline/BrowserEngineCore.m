708bafe2bdd86fbe966ff949069f4dc1d0d21e33
Berstanio <berstaniodev@gmail.com>
2025-08-05 17:33:24 +0200

    Revert "fix: Try building with old xcode again"
    This reverts commit 0d0fed16aa68c9b33aa07393b18ad15f0849048d.

---
+++ moe-core/moe.apple/moe.core.native/moe.sdk/src/inline/BrowserEngineCore.m
@@ -0,0 +1,18 @@
+//
+//  BrowserEngineCore.m
+//
+
+#import <BrowserEngineCore/BEMemory.h>
+
+
+#define EXPORT __attribute__ ((visibility ("default")))
+
+EXPORT void __natj_inline_be_memory_inline_jit_restrict_rwx_to_rw_with_witness() {
+	be_memory_inline_jit_restrict_rwx_to_rw_with_witness();
+}
+
+EXPORT void __natj_inline_be_memory_inline_jit_restrict_rwx_to_rx_with_witness() {
+	be_memory_inline_jit_restrict_rwx_to_rx_with_witness();
+}
+
+
