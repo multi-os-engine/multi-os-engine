2278258dd04cf5e7352b66e23978b77f01c5173d
Berstanio <berstaniodev@gmail.com>
2025-11-16 00:07:41 +0100

    fix: Fix invalid javadoc (again)

--- moe-core/moe.apple/moe.platform.ios/src/main/java/apple/metrickit/MXCrashDiagnostic.java
+++ moe-core/moe.apple/moe.platform.ios/src/main/java/apple/metrickit/MXCrashDiagnostic.java
@@ -260,9 +260,7 @@ public class MXCrashDiagnostic extends MXDiagnostic {
      * 
      * The MXCrashDiagnosticObjectiveCExceptionReason object associated with this crash.
      * 
-     * @see <MetricKit/MXCrashDiagnosticObjectiveCExceptionReason.h>
-     * 
-     *      API-Since: 17.0
+     * API-Since: 17.0
      */
     @Generated
     @Selector("exceptionReason")
